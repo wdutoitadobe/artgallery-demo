bdb934144522910b24073cabb75be0cb58334343
jake <jkminney@hotmail.com>
2024-08-06 10:45:00 +1000

    [Feature 2] Footer Button Decoration

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -14,7 +14,37 @@ export default async function decorate(block) {
   // decorate footer DOM
   block.textContent = '';
   const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+
+  const section = fragment.firstElementChild;
+  const children = section.children;
+  for (let i = 0; i < children.length; i++) {
+    let childElement = children[i];
+    if (childElement.classList.contains('default-content-wrapper')) {
+      footer.append(decorateFooterButton(childElement));
+    } else {
+      const newDiv = document.createElement('div');
+      newDiv.innerHTML = childElement.innerHTML;
+      footer.append(newDiv);
+    }
+  }
+
 
   block.append(footer);
+}
+
+function decorateFooterButton(footerButtonDiv) {
+
+  const footerButton = document.createElement('button');
+
+  const link = footerButtonDiv.firstElementChild.firstElementChild;
+  const buttonTitle = link.textContent;
+
+  footerButton.setAttribute('type', 'button');
+  footerButton.setAttribute('role', 'button');
+
+  footerButton.classList.add('acpl-button', 'icon', 'left', 'footer-button-to-top');
+    footerButton.innerHTML = `
+        <i class="acpl-icon utility-chevron"></i>
+        <span>${buttonTitle}</span>`;
+  return footerButton;
 }
