ae8e38bc2180c3be91167e961d6fc207b03935be
Gus Doriguetto <doriguetto@adobe.com>
2024-08-06 13:25:51 +1000

    update footer items

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,4 +1,4 @@
-import { getMetadata } from '../../scripts/aem.js';
+import {decorateBlock, getMetadata} from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
 function decorateFooterButton(footerButtonDiv) {
@@ -15,6 +15,7 @@ function decorateFooterButton(footerButtonDiv) {
   footerButton.innerHTML = `
         <i class="acpl-icon utility-chevron"></i>
         <span>${buttonTitle}</span>`;
+  footerButton.addEventListener('click', () => window.scrollTo(0, 0));
   return footerButton;
 }
 
@@ -102,6 +103,11 @@ export default async function decorate(block) {
   const footerMeta = getMetadata('footer');
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
   const fragment = await loadFragment(footerPath);
+  const sections = fragment.querySelectorAll('.section .default-content-wrapper');
+  const socialMediaSection = sections[1];
+  const disclaimerSection = sections[2];
+  const logoSection = sections[3];
+  const shieldSection = sections[4];
 
   // decorate footer DOM
   block.textContent = '';
@@ -118,4 +124,86 @@ export default async function decorate(block) {
   footer.append(decorateGroupLinks(groupLinksDiv));
 
   block.append(footer);
+
+  // -- process disclaimer and shield -- //
+  const footerBottom = document.createElement('div');
+  footerBottom.classList.add('footer-bottom');
+  footerBottom.innerHTML = `
+  <div class="footer-bottom-container content wide">
+    <div class="footer-left">
+      <div class="acpl-social-media horizontal">
+        ${Array.from(socialMediaSection.querySelectorAll('a')).map((element) => `
+          <a class="unstyled" href="${element.href}" target="_blank" title="Follow us on ${element.title}" rel="noopener noreferrer">
+            <span class="">
+              <i class="acpl-icon app-${element.title === 'twitter' ? 'x' : element.title}"></i>
+            </span>
+          </a>
+        `).join('')}
+      </div>
+    </div>
+    <div class="footer-logo">
+      ${Array.from(logoSection.querySelectorAll('picture')).map((picture) => {
+        const img = logoSection.querySelector('img');
+        img.classList.add('acpl-logo');
+        img.removeAttribute('height');
+        img.removeAttribute('width');
+        return picture.innerHTML;
+      })}  
+    </div>
+    <div class="footer-site-terms">
+      <ul class="footer-site-terms-links unstyled">
+        ${Array.from(disclaimerSection.querySelectorAll('p')).map((element) => {
+          const anchor = element.querySelector('a');
+          if (anchor) {
+            return `
+            <li>
+                <a href="${anchor.href}" target="_blank">
+                    <span>${anchor.title}</span>
+                </a>
+            </li>
+          `;
+          }
+          return `
+            <li>
+                <span>${element.textContent}</span>
+            </li>
+          `;
+        }).join('')}
+      </ul>
+      ${[shieldSection].map((element) => {
+        const paragraphs = element.querySelectorAll('p.button-container');
+        const shieldImageMetadata = paragraphs[0];
+        const iframeMetadata = paragraphs[1];
+
+        const picture = element.querySelector('picture');
+        const img = picture.querySelector('img');
+        img.setAttribute('alt', iframeMetadata.title);
+        img.setAttribute('width', 40);
+        img.setAttribute('height', 40);
+
+        return `
+        <a class="unstyled" href="${shieldImageMetadata.href}" target="_blank" title="${shieldImageMetadata.title}" rel="noopener noreferrer">
+          <span class="">
+            <i class="acpl-icon acpl-logo nz-govt"></i>
+          </span>
+        </a>
+        <a class="unstyled" href="#" target="_self" rel="">
+          <span>
+            ${picture.innerHTML}
+          </span>
+        </a>
+        <div class="acpl-modal shielded-modal" role="dialog" aria-modal="true">
+          <div class="acpl-modal-dialog">
+            <iframe src="${iframeMetadata.href}" sandbox="allow-forms allow-scripts allow-same-origin allow-popups" width="310" height="455" frameborder="0"></iframe>
+            <button type="button" role="button" class="acpl-button secondary no-border icon left no-text acpl-modal-close" aria-label="Close image">
+              <i class="acpl-icon utility-cross colored" style="background-color: rgb(115, 115, 113);"></i>
+              <span class=""></span>
+            </button>
+          </div>
+        </div>
+      `;
+  })}
+    </div>
+  </div>`;
+  footer.append(footerBottom);
 }
